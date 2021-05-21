6070d2acb7d5fc61976405b455ced3959bd22a1e
Haji Russell <hajirussell@gmail.com>
2021-05-21 10:42:51 -0400

    get fahrenheit to celsius calculator to function

---
+++ assets/js/fahrenheitcalcs.js
@@ -0,0 +1,17 @@
+"use strict";
+
+window.onload = function() {
+    const celsiusBtn = document.getElementById("celsiusBtn");
+    celsiusBtn.onclick = celsiusBtnClicked;
+}
+
+function celsiusBtnClicked() {
+    const fahrenheitDegreesFeild = document.getElementById("fahrenheitDegrees");
+    let fahrenheitDegrees = Number(fahrenheitDegreesFeild.value);
+
+    let fToC = (fahrenheitDegrees - 32) * 5/9;
+
+    const celsiusAnswerFeild = document.getElementById("celsiusAnswer");
+    celsiusAnswerFeild.value = fToC;
+
+}
